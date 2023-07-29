4d8279a080d2c54cfc525ddcd49127e33cc2fdf5
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2023-07-29 20:44:21 +0530

    Update clock-script.js

--- assets/js/clock-script.js
+++ assets/js/clock-script.js
@@ -1,1 +1,25 @@
+// Analog Clock Script
+
+function updateClock() {
+  const now = new Date();
+
+  const secondHand = document.querySelector('.second-hand');
+  const minuteHand = document.querySelector('.minute-hand');
+  const hourHand = document.querySelector('.hour-hand');
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondDegrees = (seconds / 60) * 360;
+  const minuteDegrees = ((minutes + seconds / 60) / 60) * 360;
+  const hourDegrees = ((hours + minutes / 60 + seconds / 3600) / 12) * 360;
+
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
 
